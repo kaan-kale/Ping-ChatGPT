b93d19599bdd549deb1a46d79c900f27e4115446
kaankkale <hkaankale1@gmail.com>
2023-10-01 21:31:13 +0300

    Fixed Issues

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Ping-ChatGPT
 
+- Install the openai package and add you openai api key to the environment
+
 ```bash
 pip install openai
 export OPENAI_API_KEY="YOUR_API_KEY"
+```
+
+When you run the "main.py" file, the results will be written in "results.txt". The propmt is "Count to 100, with a comma between each number and no newlines. E.g., 1, 2, 3, ..." to ensure same answer every time and solely get the response delay based on time, location etc. We do not want different answer lengths to affect our results.
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 import time
-
-import os 
+import os
+from datetime import datetime
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -13,7 +13,6 @@ def calculate_time(func):
         print(f"Time taken: {time_taken}")
         return result, time_taken
     return wrapper
-
 
 @calculate_time
 def ask_openai(prompt):
@@ -29,4 +28,18 @@ def ask_openai(prompt):
     answer = response.choices[0].message["content"]
     return answer
 
-print(ask_openai("Count to 100, with a comma between each number and no newlines. E.g., 1, 2, 3, ..."))
+
+def main():
+    # Get the current date and time
+    now = datetime.now()
+    date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    result, time_taken = ask_openai("Count to 100, with a comma between each number and no newlines. E.g., 1, 2, 3, ...")
+
+    # Write the result and date-time to the file
+    with open('results.txt', 'a') as file:
+        file.write(f'{date_time} - Time taken: {time_taken}\n')
+
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
